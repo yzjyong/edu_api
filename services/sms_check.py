0c65bdfbdfed917bdacd956fbb5b966ae749f36a
wxinSuper08 <1223241490@qq.com>
2019-06-27 02:44:49 +0800

    'flask上下文管理导致的异常'

--- services/sms_check.py
+++ services/sms_check.py
@@ -1,4 +1,6 @@
+import redis,json
 from flask import jsonify
+from apps import app
 from libs import r
 from logger import api_logger
 
@@ -8,10 +10,15 @@ def check_sms(u_phone,msg_code):
         r_msg_code = r.get('msg' + u_phone)
     except Exception as e:
         api_logger.error(e)
+        # raise Exception('数据库查询失败')
         return jsonify({'code': 202, 'msg': '数据库查询失败'})
     if not r_msg_code:
-        return jsonify({'code': 203, 'msg': '短信验证码已过期'})
+        # raise Exception('短信验证码已过期')
+        # return json.dumps({'code': 203, 'msg': '短信验证码已过期'})
+        with app.app_context():
+            return jsonify({'code': 203, 'msg': '短信验证码已过期'})
     if r_msg_code != msg_code:
+        # raise Exception('短信验证码输入错误')
         return jsonify({'code': 204, 'msg': '短信验证码输入错误'})
     return True
 
