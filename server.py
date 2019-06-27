6f199eb8c6386f2637b7ed44fef97e98cc2a104b
unknown <lyn1111_email@163.com>
2019-06-27 10:53:25 +0800

    add_cart and ucenter api first commit

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from apps import app
 from flask_cors import CORS
-from views import user_app
+from views import user_app,cart_app
 
 APP_CONFIG={
     'host': 'localhost',
@@ -11,5 +11,6 @@ APP_CONFIG={
 if __name__ == '__main__':
     CORS().init_app(app)
     app.register_blueprint(user_app.blue)
+    app.register_blueprint(cart_app.cart_blue)
 
     app.run(**APP_CONFIG)
