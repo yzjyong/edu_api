9a39ccc0e910d02dbb6e10d5723d16e3913d11e2
wxinSuper08 <1223241490@qq.com>
2019-06-29 03:36:42 +0800

    'detail更新'

--- dao/details_dao.py
+++ dao/details_dao.py
@@ -3,28 +3,31 @@ from dao import BaseDao
 
 # 详情
 class DetailsDao(BaseDao):
-    def c_list(self,*fileds,where,args):
-        return super(DetailsDao,self).list('courses',*fileds,where=where,args=args)
+    def c_list(self,fileds,where,args):
+        return super(DetailsDao,self).list('courses',fileds,where=where,args=args)
 
-    def c_top(self,course_id):  # 顶部信息
-        topcourse = self.c_list(('id','name','course_type_id','course_child_type_id','img_url'),
-                              where='course_id',args=course_id)  # 获取详情页顶部信息，图片，类名，title
-        topcourse=topcourse[0] if topcourse else None # 判定是否为空，空则返回Mone
-        return topcourse
+    def c_info(self,course_id):  # 顶部信息
+        courseinfo = self.c_list('*','course_id',course_id)  # 获取详情页顶部信息，图片，类名，title
+        return courseinfo
 
-    def c_presentation(self,c_id): # 介绍页
-        precourse = self.c_list(('description','degree','course_time','study_num','course_score',
-                               'youneed_know','teacher_tell','teacher_id'),where='id',args=c_id)[0]  # 获取课程信息
+    def c_presentation(self,course): # 介绍页
+        clist = ['id', 'name', 'course_type_id', 'course_child_type_id', 'img_url','description',
+                 'degree', 'course_time', 'study_num', 'course_score','youneed_know', 'teacher_tell',
+                 'teacher_id']
+        precourse = {k:course[k] for k in clist}
         t_info = BaseDao().list('teachers',('t_name','t_job','t_pic'),
                                     where='id',args=precourse['teacher_id'])[0]
-        return precourse, t_info
+        return {'precourse':precourse,'t_info':t_info}
 
     def c_lesson(self,c_id): # 章节页
-        lesson = BaseDao().list('lessons',('lesson_id','name'),where='course_id',args=c_id)
-        videos = BaseDao().list('videos',('video_id','name','video_url'),where='course_id',args=c_id)
-        data = {'lesson':lesson,'videos':videos}
+        lesson = self.list('lessons',('id','name'),where='course_id',args=c_id)
+        videos = self.list('videos',('name','video_url','lesson_id'),where='course_id',args=c_id)
+        data = []
+        for i in lesson:
+            i['videos'] = [j for j in videos if j['lesson_id'] == i['id']]
+            data.append(i)
         return data
 
 
 if __name__ == '__main__':
-    print(DetailsDao().c_lesson(50))
+    DetailsDao().c_lesson(50)
--- views/details_app.py
+++ views/details_app.py
@@ -6,14 +6,28 @@ blue = Blueprint('detailblue',__name__)
 # 详情页
 @blue.route('/learn/',methods=['POST'])
 def course_learn():
-    course_id = eval(request.get_data())['course_id']
+    resp = request.get_data()
+    course_id = eval(resp)['course_id'] if bool(resp) else ''
     if course_id.isdigit():
-        dao = DetailsDao()
-        topcourse = dao.get_top(course_id)
-        if topcourse:
-            topdata = {'topcourse':topcourse}
-            precourse,t_info = dao.get_presentation(course_id)
-            data = {'topdata':topdata,'precourse':precourse,'t_info':t_info}
+        courseinfo = DetailsDao().c_info(course_id)
+        if courseinfo:
+            data = DetailsDao().c_presentation(courseinfo[0])
             return jsonify(data)
         return jsonify({'code':202,'msg':'该课程不存在'})
     return jsonify({'code':202,'msg':'路由不合法'})
+
+@blue.route('/chapter/',methods=['POST'])
+def course_chapter():
+    resp = request.get_data()
+    course_id = eval(resp)['course_id'] if bool(resp) else ''
+    if course_id.isdigit():
+        courseinfo = DetailsDao().c_info(course_id)
+        if courseinfo:
+            data = DetailsDao().c_lesson(courseinfo[0]['id'])
+            return jsonify(data)
+        return jsonify({'code': 202, 'msg': '该课程不存在'})
+    return jsonify({'code': 202, 'msg': '路由不合法'})
+
+
+if __name__ == '__main__':
+    print(''.isdigit())
