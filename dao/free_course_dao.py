92d01af1617df8bf27e8d48552fe6af0268a1af6
lizhengguang1995 <824295003@qq.com>
2019-06-28 08:56:36 +0800

    'update_sql'

--- dao/free_course_dao.py
+++ dao/free_course_dao.py
@@ -1,24 +1,43 @@
 from dao import BaseDao
+from logger import api_logger
 
 
 class FreeCourseDao(BaseDao):
 
+    def type_list(self, table_name, *fileds,where=None,condition=None, args=None):
+        if not where:
+            sql = "select {} from {}".format(','.join(*fileds), table_name)
+        else:
+            sql = "select {} from {} where {}{}{}".format(','.join(*fileds), table_name,where,condition, args)
+        return self.query(sql)
+
+    # def course_type_list(self, first_table_name, second_table_name, *fileds,
+    #          where=None, args=None, frist_table_arg, second_table_arg):
+    #     if not where:
+    #         sql = "select {} from {} join {} on {}={} ".format \
+    #             (','.join(*fileds), first_table_name, second_table_name, frist_table_arg, second_table_arg)
+    #     else:
+    #         sql = "select {} from {} join {} on {}={} where {}={}".format \
+    #             (','.join(*fileds), first_table_name, second_table_name, frist_table_arg, second_table_arg, where, args)
+    #     return self.query(sql)
+
     def free_course_query(self):
         try:
-            sql1 = 'select course_id, name from courses_type '
-            sql2 = 'select courses_child_type.course_child_id, courses_child_type.name, courses_type.course_id from ' \
-                   'courses_child_type join courses_type on courses_child_type.course_type_id=courses_type.id '
-            sql3 = 'select courses.course_id, courses.name, courses.img_url, courses.is_free ,courses.degree, ' \
-                   'courses.study_num  from courses order by add_time desc limit 1, 20'
-            courses_type = self.query(sql1)
-            course_child_type = self.query(sql2)
-            courses = self.query(sql3)
+            courses_type = self.type_list('courses_type', ('course_id', 'name'))
+            print(courses_type)
+            courses_type_id = self.type_list('courses_type', ('id',), where='course_id', condition='=', args=1001)  # 获取默认大类id
+            courses_child_type = self.type_list('courses_child_type', ('course_child_id', 'name'), where='course_type_id',
+                                                condition='=',args=courses_type_id[0]['id'])  # 查询小类信息
+            print(courses_child_type)
+            courses = self.list('courses', ('course_id', 'name', 'img_url', 'is_free' ,'degree', 'study_num'),
+                                where='is_free', args=True)  # 查询大类对应课程
+            print(courses)
         except Exception as e:
             raise Exception({'code': 201, 'msg': e})
 
         return {
             "courses_type": courses_type,
-            "course_child_type": course_child_type,
+            "course_child_type": courses_child_type,
             "courses": courses,
             }
 
@@ -32,7 +51,7 @@ class FreeCourseDao(BaseDao):
                            'from courses_child_type join courses_type on courses_type.id=courses_child_type.course_type_id ' \
                            'where courses_type.course_id=%s' % type_id
                 course_sql = 'select courses.course_id, courses.name, courses.img_url, courses.is_free ,courses.degree,courses.study_num' \
-                             ' from courses join courses_type on courses.course_type_id=courses_type.id where courses_type.course_id=%s limit 1, 20' % type_id
+                             ' from courses join courses_type on courses.course_type_id=courses_type.id where courses_type.course_id=%s limit 0, 20' % type_id
                 type_message = self.query(type_sql)
                 course_message = self.query(course_sql)
             except Exception as e:
@@ -45,3 +64,6 @@ class FreeCourseDao(BaseDao):
                 return {
                     "type_message": type_message
                 }
+
+if __name__ == '__main__':
+    FreeCourseDao().free_course_query()
