47d8073812230d856a4fa9eab99feee03c1e6149
lizhengguang1995 <824295003@qq.com>
2019-07-02 17:05:20 +0800

    'address'

--- dao/__init__.py
+++ dao/__init__.py
@@ -3,7 +3,7 @@ from pymysql.cursors import DictCursor
 from logger import api_logger
 
 DB_CONFIG = {
-    "host": "localhost",
+    "host": "121.199.63.71",
     "port": 3306,
     "user": "eduadmin",
     "password": "edu7654",
